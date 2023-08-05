b8da28b40b710e8f549254d6d586bd2959a219f8
Luis <luiscorreacespedes@gmail.com>
2023-08-05 11:54:04 -0400

    Ready to deploy Back

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 import app from "./app.js";
 import { connectDB } from "./db.js";
 
-const port = process.env.PORT || 3000;
-
 connectDB();
-app.listen(port);
-app.get('/', (req, res) => {
-    res.end('it works!');
-});
-console.log('Server on port', port)
+app.listen(3000);
+console.log('Server on port', 3000)
