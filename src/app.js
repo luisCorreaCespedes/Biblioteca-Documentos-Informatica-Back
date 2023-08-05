332f9ea4360f6b91746483be51ae7542bdeffb50
Luis <luiscorreacespedes@gmail.com>
2023-08-05 00:06:59 -0400

    Ready to deploy Back

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ import cors from "cors";
 
 const app = express();
 app.use(cors({
-    origin: "https://bibliodocumentosinformatica.lol",
+    origin: "www.bibliodocumentosinformatica.lol",
     credentials: true,
     methods: ['GET', 'PUT', 'POST', 'DELETE'],
     allowedHeaders: [
